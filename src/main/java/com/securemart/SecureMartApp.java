c64d14e98b071838c5b3d161fce59ee9563e4ce5
khajavali1128 <khajavalipathan1128@gmail.com>
2024-10-25 11:04:13 -0400

    bug fix

--- src/main/java/com/securemart/SecureMartApp.java
+++ src/main/java/com/securemart/SecureMartApp.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KvEcomApplication {
+public class SecureMartApp {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(KvEcomApplication.class, args);
+        SpringApplication.run(SecureMartApp.class, args);
     }
 
 }
